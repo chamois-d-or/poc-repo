524914affff4cb02136cd81176f39725cf5c2169
chamois-d-or <89452979+chamois-d-or@users.noreply.github.com>
2022-03-04 16:04:36 +0100

    updating slices with gh action

--- utils/listSlices.js
+++ utils/listSlices.js
@@ -2,15 +2,12 @@ const glob = require("glob");
 const fetch = require('node-fetch')
 const dotenv = require('dotenv');
 dotenv.config();
-
-const slicesJsons =   glob.sync('./slices/**/model.json');
-
-console.log("slice in the github code")
-console.log(slicesJsons)
+var fs = require('fs');
 
 const ctKey = process.env.CT_API_KEY
 
 listSlices()
+pushSlices()
 
 async function listSlices() {
   const URL = `https://customtypes.prismic.io/slices`
@@ -23,8 +20,7 @@ async function listSlices() {
       "Authorization": ctKey,
       "Accept": "application/json",
       "Content-Type": "application/json",
-    },
-    //body: JSON.stringify({ query })
+    }
   }
 
   try {
@@ -33,6 +29,45 @@ async function listSlices() {
     })
 
     console.log("slices in the prismic repo before push")
+    console.log(data)
+    return data
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+async function pushSlices() {
+    const slicesJsons = glob.sync('./slices/**/model.json')
+
+    console.log("slice in the github code")
+    slicesJsons.forEach(function(slice,index){
+        const sliceObject = JSON.parse(fs.readFileSync(slicesJsons[index], 'utf8'));
+        pushSlice(sliceObject)
+    })
+}
+
+async function pushSlice(sliceObject) {
+
+  const URL = `https://customtypes.prismic.io/slices/update`
+
+  const options = {
+    endpoint: URL,
+    method: "POST", //or POST
+    headers: {
+      "repository": "poc-temp",
+      "Authorization": ctKey,
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(sliceObject)
+  }
+
+  try {
+    const data = await fetch(URL, options).then(response => {
+      return response.status
+    })
+
+    console.log("push to remote prismic repo: " + sliceObject.id)
     console.log(data)
     return data
   } catch (error) {
